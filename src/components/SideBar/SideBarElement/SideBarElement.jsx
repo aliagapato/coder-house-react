bd765c1c25d41e0304a066b8ba43c3c72f931f3d
Patricio Aliaga <aliaga.retamal.patricio@gmail.com>
2023-12-21 03:13:56 -0300

    Ajuste de evento click en componente

--- src/components/SideBar/SideBarElement/SideBarElement.jsx
+++ src/components/SideBar/SideBarElement/SideBarElement.jsx
@@ -3,11 +3,10 @@ import RandomInt from '../../../Utils/RandomInt'
 
 const SideBarElement = ({evento, id, category, activeItem}) => {
   
-  console.log(id, name, activeItem);
   return (
     <li key={id} onClick={evento} data-id={category} className={"pointer d-flex justify-content-between border rounded mb-4 p-2 fs-5 "+classes.cursorPointer+((activeItem === category) ? " border-warning" : "")}>
-      <div className="text-black">{category}</div>
-      <div className={"badge fs-6 border "+((activeItem === category) ? "bg-warning": "border-warning text-warning")}>{RandomInt()}</div>
+      <div onClick={evento} data-id={category} className="text-black">{category}</div>
+      <div onClick={evento} data-id={category} className={"badge fs-6 border "+((activeItem === category) ? "bg-warning": "border-warning text-warning")}>{RandomInt()}</div>
     </li>
   )
 
