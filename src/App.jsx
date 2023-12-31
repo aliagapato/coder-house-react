f505b4d4b0f5f125f5bed94c839cb8672d609c26
Patricio Aliaga <paliaga@colegium.com>
2023-12-30 22:19:57 -0300

    Agrego contenido a pagina de error 404

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import NavBar from './components/NavBar/NavBar'
 import CategoryContainer from './components/Containers/CategoryContainer/CategoryContainer'
 import { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import ItemListContainer from './components/Containers/ItemListContainer/ItemListContainer'
 import MainContainer from './components/Containers/MainContainer/MainContainer'
 
 function App() {
@@ -17,13 +16,12 @@ function App() {
             <CategoryContainer />
           </div>
           <div className="col col-12 col-lg-10 m-0 p-0 ps-0 ps-lg-4">
-            {/* <ItemListContainer msg="Lumos, es lo contrario de Nox" /> */}
             <MainContainer />
           </div>
         </div>
-        <div className="row m-0 p-0">
+        <div className="row m-0 mt-4 mt-lg-0 p-0">
           <div className="col m-0 p-0 border border-3 rounded text-center">
-            Fotter: Aun debo construir este componente
+            Footer: Aun debo construir este componente
           </div>
         </div>
       </div>
--- src/components/Utils/ErrorPath/ErrorPath.jsx
+++ src/components/Utils/ErrorPath/ErrorPath.jsx
@@ -1,23 +1,23 @@
 import notFound from "/notFound.png"
 
+
 const ErrorPath = () => {
   return (
-
-    <div className="border border-3 rounded p-2">
-      <div className="row">
-        <div className="col col-12 col-lg-6">
-          <img src={notFound} className="img-fluid" alt="notFound" />
+    <div className="container border border-3 mb-4 rounded p-2 text-center align-middle">
+      <div className="row justify-content-center">
+        <div className="col-6">
+          <img src={notFound} style={{ 'maxWidth': '50%', 'height': 'auto' }} alt="notFound" />
         </div>
-        <div className="col col-12 col-lg-6">
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-12">
           <h1>Error 404</h1>
         </div>
       </div>
-      <div className="row">
-        <div className="col">
-          <div>Oops! Lo sentimos, El buscador no pudo encontrar la pagina que buscas!</div>
+      <div className="row justify-content-center">
+        <div className="col-12">
+          <div>Oops! Lo sentimos, Nuestro Seeker no pudo encontrar la pagina que buscas!</div>
         </div>
-      </div>
-      <div>
       </div>
     </div>
   )
