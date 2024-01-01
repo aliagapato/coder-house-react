2756976d07596ed62001219ce46ee045659d206f
Patricio Aliaga <paliaga@colegium.com>
2023-12-31 22:45:28 -0300

    Mas ajustes de ItemCount

--- src/components/Utils/ItemCount/ItemCount.jsx
+++ src/components/Utils/ItemCount/ItemCount.jsx
@@ -30,7 +30,7 @@ const ItemCount = ({ stock, initialVulue, onAdd }) => {
     <div className="container-fluid">
       <div className="row justify-content-center">
         <div className="col col-lg-1">
-          <button onClick={remove} className="btn border border-3 btn-warning w-100">
+          <button disabled={quantity === 0} onClick={remove} className="btn border border-3 btn-warning w-100">
             <span className="fs-3 fw-bolder">-</span>
           </button>
         </div>
@@ -38,14 +38,14 @@ const ItemCount = ({ stock, initialVulue, onAdd }) => {
           <h2>{quantity}</h2>
         </div>
         <div className="col col-lg-1">
-          <button onClick={add} className="btn border border-3 btn-warning w-100">
+          <button disabled={quantity === stock} onClick={add} className="btn border border-3 btn-warning w-100">
             <span className="fs-3 fw-bolder">+</span>
           </button>
         </div>
       </div>
       <div className="row mt-2 justify-content-center">
         <div className="col col-lg-3 justify-content-center">
-          <button onClick={onAdd(quantity)} className="border border-3 btn btn-danger fw-bold w-100" type="submit"><h4>{msg.current}</h4></button>
+          <button disabled={quantity === 0} onClick={onAdd(quantity)} className="border border-3 btn btn-danger fw-bold w-100" type="submit"><h4>{msg.current}</h4></button>
         </div>
       </div>
     </div>
