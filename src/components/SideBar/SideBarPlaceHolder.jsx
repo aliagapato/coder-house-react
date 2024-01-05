935bb3af9d47c18d2f3f08e1df7266602321c597
Patricio Aliaga <paliaga@colegium.com>
2024-01-04 23:10:04 -0300

    Corrijo placeholder que murio despues de sidebar responsive con offcanvas lg

--- src/components/SideBar/SideBarPlaceHolder.jsx
+++ src/components/SideBar/SideBarPlaceHolder.jsx
@@ -1,26 +1,20 @@
 
 const SideBarPlaceHolder = () => {
   return (
-    <>
-      <p className="placeholder-glow mb-4">
-        <span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span>
-      </p>
-      <p className="placeholder-glow mb-4">
-        <span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span>
-      </p>
-      <p className="placeholder-glow mb-4">
-        <span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span>
-      </p>
-      <p className="placeholder-glow mb-4">
-        <span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span>
-      </p>
-      <p className="placeholder-glow mb-4">
-        <span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span>
-      </p>
-      <p className="placeholder-glow mb-4">
-        <span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span>
-      </p>
-    </>
+    <div className="container-fluid m-0 p-0">
+      <div className="row">
+        <p className="placeholder-glow w-100 mb-4"><span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span></p>
+        <p className="placeholder-glow w-100 mb-4"><span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span></p>
+        <p className="placeholder-glow w-100 mb-4"><span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span></p>
+        <p className="placeholder-glow w-100 mb-4"><span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span></p>
+        <p className="placeholder-glow w-100 mb-4"><span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span></p>
+        <p className="placeholder-glow w-100 mb-4"><span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span></p>
+        <p className="placeholder-glow w-100 mb-4"><span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span></p>
+        <p className="placeholder-glow w-100 mb-4"><span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span></p>
+        <p className="placeholder-glow w-100 mb-4"><span className="placeholder col-12 rounded border border-warning border-5" style={{ 'height': '48px' }} ></span></p>
+      </div>
+    </div>
+
   )
 }
 
